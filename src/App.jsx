6d536726ac0417e6907fdb361e84f5cd0f3309d6
Jonathan Maddison <jonathanwmaddison@gmail.com>
2023-03-22 03:52:05 +0000

    thanks gpt

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import OnboardingChat from './OnboardingChat';
 import WorkoutTable from './WorkoutTable';
 import useAuth from './useAuth';
+import AuthButtons from './AuthButtons'
 
 const theme = createTheme({
   // Your theme customizations
