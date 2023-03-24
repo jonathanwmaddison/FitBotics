a361931dd1e00ffbcb2060cd891006038ce077d2
Jonathan Maddison <jonathanwmaddison@gmail.com>
2023-03-24 03:06:06 +0000

    fix

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -28,27 +28,31 @@ exports.chat = functions.https.onRequest( async (request, response) => {
     response.status(204).send("");
   }
 
-  // if (request.httpMethod !== "POST") {
-  //   return response.send({statusCode: 405, body: "Method Not Allowed"});
-  // }
-  functions.logger.info(request.body, {structuredData: true});
 
   const { prompt } = request.body;
+  functions.logger.info(prompt, {structuredData: true});
+
   try {
     const {data} = await openai.createChatCompletion(
         {
           "model": "gpt-3.5-turbo",
           "messages": [
+            {"role": "system", "content": "You are an assistant that helps create workout plans and can provide them in structured formats" },
             {"role": "user", "content": prompt },
           ],
           "max_tokens": 500,
         });
-    functions.logger.info(data, {structuredData: true});
+    functions.logger.info(data.choices, {structuredData: true});
     if (data.choices && data.choices.length > 0) {
       return response.send({
         statusCode: 200,
-        body: data.choices[0].messsage.content.trim(),
+        body: data.choices[0].messsage.content,
       });
+    }
+    else {
+      return response.send({
+        statusCode: 500,
+        body: "The prompt did not have a response"});
     }
   } catch (error) {
     functions.logger.error("Error generating response:", error);
@@ -56,9 +60,5 @@ exports.chat = functions.https.onRequest( async (request, response) => {
       statusCode: 500,
       body: "An error occurred while generating a response.",
     });
-  } finally {
-    response.send({
-      statusCode: 500,
-      body: "The prompt did not have a response"});
-  }
+  } 
 });
