d6ab3d09d27a04b5f3cbc7b90be4fd7f68dcf062
Jonathan Maddison <jonathanwmaddison@gmail.com>
2023-03-25 02:25:29 +0000

    use tailwind

--- src/OnboardingChat.jsx
+++ src/OnboardingChat.jsx
@@ -1,14 +1,15 @@
 // OnboardingChat.js
 import React, { useState } from 'react';
 import axios from 'axios';
-import { TextField, Button } from '@mui/material';
 import { useSaveWorkoutPlan } from './hooks/useSaveWorkoutPlan';
 function OnboardingChat({ onWorkoutPlanGenerated }) {
   const [userData, setUserData] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [chatMessages, setChatMessages] = useState([]);
+
+  const userQueries = ["goals", "age", "fitness level"];
   const { saveWorkoutPlan, saving, error } = useSaveWorkoutPlan();
 
-  const userQueries = ["goals", "age", "fitness level"]
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
@@ -21,6 +22,11 @@ function OnboardingChat({ onWorkoutPlanGenerated }) {
 
   const handleSubmit = async () => {
     if (inputValue.trim() === '') return;
+
+    setChatMessages((prevChatMessages) => [
+      ...prevChatMessages,
+      { type: 'user', message: inputValue },
+    ]);    
     setUserData([...userData, inputValue]);
     setInputValue('');
 
@@ -49,21 +55,54 @@ function OnboardingChat({ onWorkoutPlanGenerated }) {
       }
     }
   };
-
+  useEffect(() => {
+    if (userData.length < 3) {
+      setChatMessages((prevChatMessages) => [
+        ...prevChatMessages,
+        {
+          type: 'bot',
+          message: `Please enter your ${userQueries[userData.length]}:`,
+        },
+      ]);
+    }
+  }, [userData]);
   return (
-    <div>
-      <TextField 
-        label={userQueries[userData.length] || 'loading plan'}
-        variant="outlined"
-        value={inputValue}
-        onChange={handleInputChange}
-        onKeyPress={handleKeyPress}
-      />
-      <Button disabled={!!userData.length > 2} variant="contained" color="primary" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </div>
-  );
+    
+      <div className="bg-gray-100 min-h-screen p-4">
+        <div className="max-w-xl mx-auto">
+          <div className="bg-white rounded-xl p-4 space-y-4">
+            {chatMessages.map((chatMessage, index) => (
+              <div
+                key={index}
+                className={`chat-message ${
+                  chatMessage.type === 'user' ? 'chat-author' : 'chat-bot'
+                }`}
+              >
+                <div className="flex-none">{chatMessage.type === 'user' ? 'You' : 'Bot'}</div>
+                <div className="text-sm">{chatMessage.message}</div>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col items-center space-y-4 mt-4">
+            <input
+              className="input input-bordered w-full"
+              type="text"
+              placeholder="Type your answer here..."
+              value={inputValue}
+              onChange={handleInputChange}
+              onKeyPress={handleKeyPress}
+            />
+            <button
+              className={`btn btn-primary ${userData.length > 2 ? 'opacity-50' : ''}`}
+              onClick={handleSubmit}
+              disabled={userData.length > 2}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </div>
+    );
 }
 
 export default OnboardingChat;
