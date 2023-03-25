cb8baf95e1e2b49339ab725c5544100631d10a1f
Jonathan Maddison <jonathanwmaddison@gmail.com>
2023-03-25 04:28:42 +0000

    refactor

--- src/hooks/useSaveWorkoutPlan.js
+++ src/hooks/useSaveWorkoutPlan.js
@@ -1,19 +1,17 @@
 import { useState } from 'react';
 import { getFirestore, collection, addDoc, getDoc } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import useAuth from '../useAuth';
 
 export const useSaveWorkoutPlan = () => {
   const [saving, setSaving] = useState(false);
   const [error, setError] = useState(null);
-
+const { user } = useAuth()
   const saveWorkoutPlan = async (workoutPlan) => {
     setSaving(true);
     setError(null);
 
     try {
-      const db = getFirestore();
-      const auth = getAuth();
-      const user = auth.currentUser;
 
       if (user) {
         await addDoc(collection(db, 'workoutPlans'), {
@@ -21,6 +19,8 @@ export const useSaveWorkoutPlan = () => {
           userId: user.uid,
           workoutPlan,
         });
+
+        
       } else {
         throw new Error('User not authenticated');
       }
