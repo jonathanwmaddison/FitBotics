09462167c2331cc31c8f4a0f4c8d6a5161d07e33
Jonathan Maddison <jonathanwmaddison@gmail.com>
2023-03-25 12:02:41 +0000

    imroove workout table

--- src/WorkoutTable.jsx
+++ src/WorkoutTable.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function WorkoutTable({ workoutPlan }) {
-  const renderExerciseRows = (exercises) => {
-    return exercises.map((exercise, index) => (
-      <React.Fragment key={index}>
-        <td>{exercise.name}</td>
-        <td>{exercise.reps}</td>
-      </React.Fragment>
-    ));
+  const [activeDays, setActiveDays] = useState({});
+
+  const toggleDay = (index) => {
+    setActiveDays((prevActiveDays) => {
+      const newActiveDays = { ...prevActiveDays };
+      if (newActiveDays[index]) {
+        delete newActiveDays[index];
+      } else {
+        newActiveDays[index] = true;
+      }
+      return newActiveDays;
+    });
+  };
+
+  const renderExercises = (exercises) => {
+    return (
+      <div className="border-t border-gray-200 dark:border-gray-700">
+        {exercises.map((exercise, index) => (
+          <div key={index} className="flex justify-between px-4 py-2 text-gray-700 dark:text-gray-300">
+            <div className="max-w-md ">{exercise.name}</div>
+            <div className="max-w-xs ">{exercise.reps}</div>
+          </div>
+        ))}
+      </div>
+    );
   };
 
   const renderWorkoutPlanRows = () => {
@@ -16,29 +34,26 @@ function WorkoutTable({ workoutPlan }) {
     }
 
     return workoutPlan.workouts.map((workout, index) => (
-      <tr key={index}>
-        <td>{workout.week}</td>
-        <td>{workout.day}</td>
-        {renderExerciseRows(workout.exercises)}
-        <td>{workout.description}</td>
-      </tr>
+      <div key={index} className="border border-gray-200 dark:border-gray-700 mb-2">
+        <div
+          className="flex justify-between px-4 py-2 bg-gray-100 dark:bg-gray-800 cursor-pointer text-gray-700 dark:text-gray-300"
+          onClick={() => toggleDay(index)}
+        >
+          <div className="max-w-xs ">Day {workout.day}</div>
+          <div className="max-w-lg ">{workout.description}</div>
+        </div>
+        {activeDays[index] && renderExercises(workout.exercises)}
+      </div>
     ));
   };
 
   return (
-    <div className="overflow-x-auto">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            <th>Week</th>
-            <th>Day</th>
-            <th>Exercise</th>
-            <th>Reps</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>{renderWorkoutPlanRows()}</tbody>
-      </table>
+    <div>
+      <div className="flex justify-between px-4 py-2 font-bold text-gray-700 dark:text-gray-300">
+        <div>Day</div>
+        <div>Description</div>
+      </div>
+      {renderWorkoutPlanRows()}
     </div>
   );
 }
