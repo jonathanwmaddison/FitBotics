2a03630fe3c6d534cea38e8e8a175852baccd0ed
Jonathan Maddison <jonathanwmaddison@gmail.com>
2023-03-25 03:46:12 +0000

    fix

--- src/hooks/useSavedPlan.js
+++ src/hooks/useSavedPlan.js
@@ -1,27 +1,51 @@
-import { useCallback, useEffect, useState } from 'react'
-import { getFirestore, collection, addDoc, getDoc } from 'firebase/firestore';
+import { useCallback, useEffect, useState } from 'react';
+import { getFirestore, collection, query, where, onSnapshot } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-import useAuth from '../useAuth';
 const useSavedPlan = () => {
-    const { user } = useAuth();
-    const [workoutPlan, setWorkoutPlan] = useState([])
+  const [workoutPlan, setWorkoutPlan] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const getWorkoutPlan = useCallback(() => {
-        const workoutPlanRef = getDoc(collection('users', user.id, 'workoutPlans'))
-        const unsubscribe = workoutPlanRef.onSnapshot((doc) => {
-            if (doc.exists) {
-                setWorkoutPlan(doc.data().workoutPlan);
-            }
-        });
-        return () => unsubscribe();
-    }, [user])
+  const auth = getAuth();
+  const user = auth.currentUser;
 
-    useEffect(() => {
-        if (!user) return;
+  const getWorkoutPlan = useCallback(() => {
+    setLoading(true);
+    setError(null);
 
-        return getWorkoutPlan()
-    
-    }, [user])
-    return [workoutPlan]
-}
+    const db = getFirestore();
+    const workoutPlanRef = query(
+      collection(db, 'workoutPlans'),
+      where('userId', '==', user.uid),
+      // You can add more filters here if needed
+    );
+    const unsubscribe = onSnapshot(workoutPlanRef, (snapshot) => {
+      const plans = [];
+      snapshot.forEach((doc) => {
+        plans.push(doc.data().workoutPlan);
+      });
+      if (plans.length > 0) {
+        setWorkoutPlan(plans[0]); // Only return the first plan
+      } else {
+        setWorkoutPlan(null);
+      }
+      setLoading(false);
+    }, (err) => {
+      setError(err.message);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const unsubscribe = getWorkoutPlan();
+    return unsubscribe;
+  }, [user, getWorkoutPlan]);
+
+  return { workoutPlan, loading, error };
+};
+
 export default useSavedPlan;
